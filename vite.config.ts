d2d1221a0cbcc1469d0598f7b08949a8a9c568b1
aryanseth9795 <iiitianaryan@gmail.com>
2026-05-17 18:50:04 +0530

    deployment error fix

--- vite.config.ts
+++ vite.config.ts
@@ -12,11 +12,6 @@ export default defineConfig({
   },
   server: {
     port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-      },
-    },
+    
   },
 });
